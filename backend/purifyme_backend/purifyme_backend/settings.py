ae20c9d14bc35327560e504e6dd19c93852642f4
Taner-glitch <taner.eren2003@gmail.com>
2025-05-25 15:54:56 +0300

    Update settings.py

--- backend/purifyme_backend/purifyme_backend/settings.py
+++ backend/purifyme_backend/purifyme_backend/settings.py
@@ -8,8 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#-42c8jr*wb2z_=a0#&3-8vjs8)+xj=q)&6i6q)5_r594j71dm'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
